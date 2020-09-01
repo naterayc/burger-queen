3026dd32c2509389093be8c03f4413639253ce92
yndira <yndinatera10@gmail.com>
2020-09-01 17:46:23 -0400

    changes2

--- src/components/templateSalon/salon.js
+++ src/components/templateSalon/salon.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './templateSalon.css'
 
 const Salon = () => {
   return (
     <div>
       <h1>Esta sera la pantalla de Salon</h1>
       <button>
-        <Link to="/"> Kitchen 
-        </Link>
+        <Link to="/"> Kitchen </Link>
       </button>
     </div>
   );
