4d0978af2fc7677fc6589cbd4d6870227dfc4aca
yndira <yndinatera10@gmail.com>
2020-09-02 13:04:21 -0400

    delete unused vars

--- src/components/templateGarzon/garzon.js
+++ src/components/templateGarzon/garzon.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState }from 'react';
+import React, { Fragment }from 'react';
 import './garzon.css';
 import ItemsMenu from './items-menu';
 
--- src/components/templatekitchen/kitchen.js
+++ src/components/templatekitchen/kitchen.js
@@ -1,10 +1,9 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 const Kitchen = () => {
-  const usuario = JSON.parse(sessionStorage.getItem('user'));
   return (
     <Fragment>
-      <h1 className="identification"> Chef: {usuario.user} </h1>
+      <h1> Cocina </h1>
       <button>
         <Link to="/">Ir al inicio</Link>
       </button>
