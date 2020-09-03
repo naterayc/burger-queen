536678abd22091d3ef276e9a5524184a88d32b01
yndira <yndinatera10@gmail.com>
2020-09-02 21:41:57 -0400

    layout template kitchen

--- src/components/templatekitchen/kitchen.js
+++ src/components/templatekitchen/kitchen.js
@@ -1,12 +1,21 @@
 import React, { Fragment } from "react";
+import './identificationKitchen.css';
+import LogoKitchen from "./logo-kitchen";
 import { Link } from "react-router-dom";
+
 const Kitchen = () => {
+
+  const usuario = JSON.parse(sessionStorage.getItem('user'));
+
   return (
+
     <Fragment>
-      <h1> Cocina </h1>
-      <button>
-        <Link to="/">Ir al inicio</Link>
-      </button>
+      <h1 className="title-kitchen"> Chef: {usuario.user} </h1>
+      <h2 className="kitchen">Cocina</h2>
+      <h3 className="orders">Pedidos</h3>
+      <Link to="/area">
+        <LogoKitchen/>
+      </Link>
     </Fragment>
   );
 };
