4d55d0402b168f7ace3d117ad3f585a86586d64f
yndira <yndinatera10@gmail.com>
2020-09-09 20:19:14 -0300

    add funcionality to button, changes the states(needs to be fixed)

--- src/components/templateKitchen/orders.js
+++ src/components/templateKitchen/orders.js
@@ -1,24 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './order.css';
 import ItemInOrder from './itemInOrder';
+import BtnState from './btn-state'
 
 const Order = (props) => {
+
+    let [btnText, setBtnText] = useState("Preparar");
+    let [btnClass, setBtnClass] = useState("btn-prepair");
+
+    const setFinish = (e) => {
+
+        if (props.orders.filter(item => item === e.target.id)) {
+            setBtnText("Terminar");
+            setBtnClass("btn-finish");
+        }
+    }
+
     return (
-     props.orders.map(orders => {
-            return (           
-            <Fragment key={orders.mesa}>
-                <div className="container-order">
-                    <h4 className="table-number">{orders.mesa}</h4>
-                    
-                    <ItemInOrder ordersKitchen={orders}/>
-                    
-                    <div className="center">
-                        <button className="btn-prepair">Preparar</button>
+        props.orders.map(orders => {
+            return (
+                <Fragment key={orders.mesa}>
+                    <div className="container-order">
+                        <h4 className="table-number">{orders.mesa}</h4>
+
+                        <ItemInOrder
+                            ordersKitchen={orders}
+                        />
+                        <div key={orders.id} className="center" >
+                            <button
+                                id={orders.id}
+                                className={btnClass}
+                                onClick={setFinish} >
+                                {btnText}
+                            </button>
+                        </div>
+
+
                     </div>
-                </div>
-            </Fragment>
-         )
-        }) 
+                </Fragment>
+            )
+        })
     );
 }
 
