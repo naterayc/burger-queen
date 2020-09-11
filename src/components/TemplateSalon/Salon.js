bc2264467f264b5cd0c08a31a81fe50f45d162e9
danielablancom <blancodanielac@gmail.com>
2020-09-11 13:24:51 -0300

    templateSalon and uploaded icon table to component

--- src/components/TemplateSalon/Salon.js
+++ src/components/TemplateSalon/Salon.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import LogoSmall from "../logo-small";
 
-const Salon = () => {
+
+const salon = () => {
+
+    const usuario = JSON.parse(sessionStorage.getItem("user"));
+     
   return (
-    <div className="container-parent2">
-      <h1>Esta sera la pantalla de Salon</h1>
-      <button>
-        <Link to="/area"> Kitchen </Link>
-      </button>
-    </div>
+    <Fragment>
+      <div className="container-parent2">
+        <h1 classeName="title-kitchen"> Chef: {usuario.user} </h1>
+        <h2 className="kitchen">Salón</h2>
+        <h3 className="orders">Pedidos listos para servir</h3>
+        <Link to="/area">
+          <LogoSmall />
+        </Link>
+      </div>
+    </Fragment>
   );
 };
-export default Salon;
+
+export default salon;
+ 
---
+++ src/components/TemplateSalon/icon-table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import icon-table from '';
+
+
+const insertIconTable = () => {
+   return (
+        <Fragment>
+            <img src={icon-table} className="icon-table" alt="icon"/>
+        </Fragment>
+   ); 
+}
