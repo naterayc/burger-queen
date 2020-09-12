9befdacefb36e495d8f673dacef9f9c98d0fb799
yndira <yndinatera10@gmail.com>
2020-09-11 21:15:19 -0300

    add funcionality to render ordersToServe, add funcionality to delete orderSeved in firebase

--- src/components/TemplateSalon/Salon.js
+++ src/components/TemplateSalon/Salon.js
@@ -1,9 +1,47 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import LogoSmallSalon from "./logo-salon";
 import InsertIconTable from "./icon-table";
+import { firebase } from '../../firebase';
 
 const Salon = () => {
   const usuario = JSON.parse(sessionStorage.getItem("user"));
+  let [ordersToServe, setOrdersToServe] = useState([]);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const db = firebase.firestore();
+        const data = await db.collection('orders').where("hora", "==", "terminado")
+          .get()
+        data.docs.map(doc => {
+          setOrdersToServe(ordersToServe = [...ordersToServe, ({ id: doc.id, ...doc.data() })])
+
+          console.log(ordersToServe);
+        })
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+    getOrders();
+  }, [])
+
+  const deleteOrder = async (e) => {
+    e.preventDefault();
+    let id = e.currentTarget.id;
+
+    try {
+      const db = firebase.firestore();
+      await db.collection('orders').doc(id).delete()
+    } catch (error) {
+      console.log(error);
+    }
+
+
+    const arrOrderToServe = ordersToServe.filter(order => order.id !== id);
+      setOrdersToServe(arrOrderToServe);
+  }
+
 
   return (
     <Fragment>
@@ -15,10 +53,7 @@ const Salon = () => {
           <LogoSmallSalon />
         </div>
         <div className="container-table">
-          <InsertIconTable />
-          <InsertIconTable />
-          <InsertIconTable />
-          <InsertIconTable />
+          <InsertIconTable delete={deleteOrder} orders={ordersToServe} />
         </div>
       </div>
     </Fragment>
--- src/components/TemplateSalon/icon-table.js
+++ src/components/TemplateSalon/icon-table.js
@@ -3,14 +3,18 @@ import './templateSalon.css';
 import imgOrders from '../../img/imgOrders.png';
 
 
-const InsertIconTable = () => {
-   return (
-     <Fragment>
-       <div className="icon-table">
-           <p className="number-table">2</p>
-         <img src={imgOrders} className="img-table" alt="icon" />
-       </div>
-     </Fragment>
-   ); 
+const InsertIconTable = (props) => {
+  return (
+    props.orders.map(orders => {
+      return (
+        <Fragment>
+          <div className="icon-table" id={orders.id} onClick={props.delete}>
+            <p className="number-table">{orders.mesa}</p>
+            <img src={imgOrders} className="img-table" alt="icon" />
+          </div>
+        </Fragment>
+      )
+    })
+  )
 }
 export default InsertIconTable;
