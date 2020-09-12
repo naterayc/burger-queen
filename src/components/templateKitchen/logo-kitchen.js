d5ae37982b07df4853bdc94175d53814c14cd450
yndira <yndinatera10@gmail.com>
2020-09-11 21:12:15 -0300

    add link

--- src/components/templateKitchen/logo-kitchen.js
+++ src/components/templateKitchen/logo-kitchen.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoSmall from '../../img/logoSmall.png'
 import './logoKitchen.css';
 
 const LogoKitchen = () => {
     return (
         <div className="container-logo-kitchen">
-            <img src={logoSmall} className="logo-kitchen" alt="logo" />
+            <Link to="/area"> <img src={logoSmall} className="logo-kitchen" alt="logo" /> </Link>
         </div>
     );
 }
